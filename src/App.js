e1a9de550017b8ed57013d387cb564dfd7bf2c85
solahidris <emailsolah@gmail.com>
2023-07-20 00:12:28 +0800

    added code plan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// MAIN BRANCH
+
+import CodeHow from "./components/CodeHow";
+import HeaderTitle from "./components/HeaderTitle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="min-h-[100vh] bg-gradient-to-b from-indigo-100 from-10% via-sky-300 via-60% to-emerald-300 to-90%">
+      <h5 className="text-xs">this is the main branch</h5>
+
+      <div className="p-5">
+        <HeaderTitle />
+        <CodeHow />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// const questions = [
+//   {
+//     question: "What is the capital of France?",
+//     options: [
+//       { id: 1, text: "Paris", isCorrect: true },
+//       { id: 2, text: "London", isCorrect: false },
+//       { id: 3, text: "Madrid", isCorrect: false },
+//       { id: 4, text: "Berlin", isCorrect: false }
+//     ]
+//   },
+//   // Add more question objects here
+// ];
+
+// {question.options.map((option) => (
+//   <label key={option.id}>
+//     <input
+//       type="radio"
+//       name="answer"
+//       value={option.id}
+//       onChange={handleAnswerSelection}
+//     />
+//     {option.text}
+//   </label>
+// ))}
---
+++ src/components/HeaderTitle.js
@@ -0,0 +1,15 @@
+const HeaderTitle = () => {
+  return (
+    <div className="bg-stone-100/50">
+      <h1 class="text-3xl font-bold underline">P20 - React Quiz App</h1>
+      <h2 class="text-sm">
+        Build a simple React quiz app where users can click on option buttons to
+        answer questions, next question rendered each time option is selected
+        and at the end, the final score is displayed.
+      </h2>
+      <br />
+    </div>
+  );
+};
+
+export default HeaderTitle;
