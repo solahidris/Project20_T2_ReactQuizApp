6b050474aee69930a505b9d2529aaab497a98faa
solahidris <emailsolah@gmail.com>
2023-07-22 00:14:00 +0800

    center text pt2

--- src/components/QuizApp.js
+++ src/components/QuizApp.js
@@ -35,12 +35,22 @@ const QuizApp = () => {
     <div className="bg-blue-200 px-[2rem] py-[3rem] mx-5 lg:mx-[30%] my-[2rem] rounded-xl">
       {/* Render current question or final message */}
       {currentQuestionIndex !== -1 ? (
-        <div key={currentQuestionIndex} className="bg-blue-300 px-[1rem] rounded-xl">
-          <p className="pt-5 text-xs italic text-end"># {currentQuestionIndex + 1} / {qna.length}</p>
-          <p className="text-center font-bold bg-blue-200 m-5 p-5 rounded-xl">{currentQuestion.question}</p>
+        <div
+          key={currentQuestionIndex}
+          className="bg-blue-300 px-[1rem] rounded-xl"
+        >
+          <p className="pt-5 text-xs italic text-end">
+            # {currentQuestionIndex + 1} / {qna.length}
+          </p>
+          <p className="text-center font-bold bg-blue-200 m-5 p-5 rounded-xl">
+            {currentQuestion.question}
+          </p>
           <ul className="pb-5">
             {currentQuestion.answer.map((answer) => (
-              <label key={answer.id} className="flex rounded-full bg-blue-500 hover:bg-blue-600 text-white py-2 text-xs mb-2 item-center">
+              <label
+                key={answer.id}
+                className="flex rounded-full bg-blue-500 hover:bg-blue-600 text-white py-2 text-xs mb-2 item-center"
+              >
                 <input
                   type="radio"
                   name={`question_${currentQuestionIndex}`}
@@ -49,17 +59,27 @@ const QuizApp = () => {
                   onChange={() => handleAnswerSelection(answer.id)}
                   style={{ display: "none" }} // Hide the radio input
                 />
-                <div className="mx-4">
-                  {answer.text}
-                </div>
+                <div className="mx-4">{answer.text}</div>
               </label>
             ))}
           </ul>
         </div>
       ) : (
         <div>
-          <div className="flex justify-center"><p className="flex bg-blue-500 text-white px-5 py-3 rounded-full text-center">All questions answered! Your score: {correctAnswerCounter}</p></div>
-          <div className="flex justify-center"><button onClick={handleTryAgain} className="bg-stone-500 hover:bg-stone-600 text-white text-sm px-5 py-2 mt-4 rounded-full">Try Again</button></div>
+          <div className="flex justify-center">
+            <div className="flex flex-col bg-blue-500 text-white px-5 py-3 rounded-full text-center">
+              <p>All questions answered!</p>
+              <p> Your score: {correctAnswerCounter}</p>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              onClick={handleTryAgain}
+              className="bg-stone-500 hover:bg-stone-600 text-white text-sm px-5 py-2 mt-4 rounded-full"
+            >
+              Try Again
+            </button>
+          </div>
         </div>
       )}
     </div>
